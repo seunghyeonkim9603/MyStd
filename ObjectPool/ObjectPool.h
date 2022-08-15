232bf66b577554199c218508ed532a32c8694459
seunghyeonkim9603 <seunghyeonkim9603@gmail.com>
2022-08-15 23:26:11 +0900

    Add "Placement New" feature enabled or disabled to be taken from the constructor

--- ObjectPool/ObjectPool.h
+++ ObjectPool/ObjectPool.h
@@ -6,7 +6,7 @@ template<typename T>
 class ObjectPool final
 {
 public:
-	ObjectPool(size_t size);
+	ObjectPool(size_t size, bool bIsPlacementNew);
 	ObjectPool(ObjectPool& other) = delete;
 	ObjectPool(ObjectPool&& other) = delete;
 	~ObjectPool();
@@ -16,35 +16,59 @@ public:
 
 	void ReleaseObject(T* obj);
 
-	void GetAllCount() const;
-	void GetActiveCount() const;
-	void GetInactiveCount() const;
+	size_t GetAllCount() const;
+	size_t GetActiveCount() const;
+	size_t GetInactiveCount() const;
 
 private:
 	std::vector<char*> mPool;
 	size_t mAllCount;
 	size_t mActiveCount;
+	bool mbIsPlacementNew;
 };
 
 template<typename T>
-inline ObjectPool<T>::ObjectPool(size_t size)
+inline ObjectPool<T>::ObjectPool(size_t size, bool bIsPlacementNew)
 	: mAllCount(size),
-	mActiveCount(0)
+	mActiveCount(0),
+	mbIsPlacementNew(bIsPlacementNew)
 {
 	mPool.reserve(size);
 
-	for (size_t i = 0; i < size; ++i)
+	if (bIsPlacementNew)
 	{
-		mPool.push_back(new char[sizeof(T)]);
+		for (size_t i = 0; i < size; ++i)
+		{
+			mPool.push_back(new char[sizeof(T)]);
+		}
+	}
+	else
+	{
+		for (size_t i = 0; i < size; ++i)
+		{
+			mPool.push_back(reinterpret_cast<char*>(new T()));
+		}
 	}
 }
 
 template<typename T>
 inline ObjectPool<T>::~ObjectPool()
 {
-	for (int i = 0; i < mPool.size(); ++i)
+	if (mbIsPlacementNew)
 	{
-		delete mPool[i];
+		for (int i = 0; i < mPool.size(); ++i)
+		{
+			delete mPool[i];
+		}
+	}
+	else
+	{
+		for (int i = 0; i < mPool.size(); ++i)
+		{
+			T* obj = reinterpret_cast<T*>(mPool[i]);
+
+			delete obj;
+		}
 	}
 }
 
@@ -62,34 +86,38 @@ inline T* ObjectPool<T>::GetObject(TArgs&&... args)
 	T* obj = reinterpret_cast<T*>(mPool.back());
 	mPool.pop_back();
 
-	obj = new (obj) T(std::forward<TArgs>(args)...);
-
+	if (mbIsPlacementNew)
+	{
+		obj = new (obj) T(std::forward<TArgs>(args)...);
+	}
 	return obj;
 }
 
 template<typename T>
 inline void ObjectPool<T>::ReleaseObject(T* obj)
 {
-	obj->T::~T();
-
+	if (mbIsPlacementNew)
+	{
+		obj->T::~T();
+	}
 	mPool.push_back(reinterpret_cast<char*>(obj));
 	--mActiveCount;
 }
 
 template<typename T>
-inline void ObjectPool<T>::GetAllCount() const
+inline size_t ObjectPool<T>::GetAllCount() const
 {
 	return mAllCount;
 }
 
 template<typename T>
-inline void ObjectPool<T>::GetActiveCount() const
+inline size_t ObjectPool<T>::GetActiveCount() const
 {
 	return mActiveCount;
 }
 
 template<typename T>
-inline void ObjectPool<T>::GetInactiveCount() const
+inline size_t ObjectPool<T>::GetInactiveCount() const
 {
 	return mAllCount - mActiveCount;
 }
